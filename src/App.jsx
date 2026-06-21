0d9e2f37d7bea07b8d283c861996b9fdb073867e
saanchijainnn <saanchi2315@gmail.com>
2026-06-21 16:29:01 +0530

    use of async await

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import axios from 'axios'
 
 const App = () => {
+
+const getdata=async()=>{
+  console.log("data")
+  const response = await axios.get("https://picsum.photos/v2/list?page=2&limit=100")
+
+  console.log(response)
+  }
+
   return (
-    <div>App</div>
+    <div className="bg-black h-screen p-4 text-white">
+      <button 
+      onClick={getdata }
+      className="bg-green-600 mb-3 active:scale-95text-white  py-2 px-5 rounded">
+        Get data
+      </button>
+    </div>
   )
 }
 
